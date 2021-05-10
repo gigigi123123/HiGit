c0332eabceb518853a573e568cb32f0fa54eb7c5
Lebron Zheng <2207549516@qq.com>
2021-05-10 23:12:32 +0800

    test

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @program: hello
+ * @ClassName Hello
+ * @author: LeBron
+ * @create: 2021-05-10 23:10
+ * @description:
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World.....");
+    }
+}
